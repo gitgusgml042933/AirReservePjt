a6aad06aadd322072e5eb0f75428a36e4c1b126c
gitgusgml042933 <gitgusgml042933@gmail.com>
2023-05-30 19:35:17 +0900

    1차 서비스 완료

--- reserve_db.js
+++ reserve_db.js
@@ -1,5 +1,6 @@
 //비행기 예약 DB
 let reserve_db = new Map();
 
-let reserve_dummy = new Reserve('1', '1', '1', '1', '1', '1', '1');
-reserve_db.set('uni', reserve_dummy);
+let reserve_dummy = new Reserve('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+
+reserve_db.set('1', reserve_dummy);
