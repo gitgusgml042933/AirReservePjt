63a7016f752b485e0c801eb5f6488e4e61384609
jangdebug2 <jangdebug2@gmail.com>
2023-05-30 17:14:35 +0900

    db 오류 수정

--- reserve_db.js
+++ reserve_db.js
@@ -1,7 +1,8 @@
 //비행기 예약 DB
 let reserve_db = new Map();
 
-let reserve_dummy = new Reserve('1', '1', '1', '1', '1', '1', '1');
+let reserve_dummy = new Reserve('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+
 reserve_db.set('uni', reserve_dummy);
 
 
